f36af782543487fce550a45927793e577f6ced94
Ralph Ramirez <115379467+Rafz7777@users.noreply.github.com>
2024-11-18 20:26:13 -0800

    Add files via upload

---
+++ lib/splace_scren/splace_scren_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'splace_scren_widget.dart' show SplaceScrenWidget;
+import 'package:flutter/material.dart';
+
+class SplaceScrenModel extends FlutterFlowModel<SplaceScrenWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
